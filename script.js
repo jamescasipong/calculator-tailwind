841410b78e0494850e9988bb25dccb94b65c09e3
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-02 21:49:18 +0800

    update yeah

--- script.js
+++ script.js
@@ -21,3 +21,31 @@ function calculateResult() {
     displayValue = ""; // Reset display on error
   }
 }
+
+let getBodyId;
+let isLight = true;
+let element;
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Code here will run after the DOM is fully loaded and parsed
+  getBodyId = document.getElementById("light-mode");
+  element = document.getElementById("toggleMode");
+});
+
+function toggleTheme(){
+  if (!getBodyId) {
+    console.error("Element with id 'light-mode' not found.");
+    return;
+  }
+
+  if(isLight){
+    getBodyId.id = "dark-mode";
+    element.innerHTML = "Light MODE";
+    isLight = false;
+    console.log(isLight);
+  } else {
+    getBodyId.id = "light-mode";
+    element.innerHTML = "DAY MODE";
+    isLight = true;
+  }
+}
